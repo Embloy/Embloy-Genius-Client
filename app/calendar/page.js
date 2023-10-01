f606a36c7a12dc961ac5542869289867f85636a2
github4touchdouble <touchedouble@gmx.de>
2023-10-01 22:36:51 +0200

    added basic calendar functionality

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -20,7 +20,7 @@ export default function Calendar() {
 
 
 
-    const [events, setEvents] = useState([
+    const [events, setShowEvents] = useState([
         {title: 'event 1', id: '1', start: '2023-08-29 06:16:28', end: '2023-08-29 16:00:00'},
         {title: 'event 2', id: '2', start: '2023-08-30 06:16:28', end: '2023-08-30 16:00:00'},
         {title: 'event 3', id: '3', start: '2023-08-31 06:16:28', end: '2023-08-31 16:00:00'},
@@ -41,6 +41,13 @@ export default function Calendar() {
     const [showDeleteModal, setShowDeleteModal] = useState(false)
     const [idToDelete, setIdToDelete] = useState(null)
     const [newEvent, setNewEvent] = useState({
+        title: '',
+        start: '',
+        end: '',
+        id: 0,
+    })
+    const [showEventModal, setShowEventModal] = useState(false)
+    const [showEvent, setShowEvent] = useState({
         title: '',
         start: '',
         end: '',
@@ -107,6 +114,31 @@ export default function Calendar() {
         setIdToDelete(Number(data.event.id))
     }
 
+    function formatDateTime(data) {
+        const binDate = new Date(data);
+        const binYear = binDate.getFullYear();
+        const binMonth = String(binDate.getMonth() + 1).padStart(2, '0'); // Month is 0-based
+        const binDay = String(binDate.getDate()).padStart(2, '0');
+        const binHour = String(binDate.getHours()).padStart(2, '0');
+        const binMinute = String(binDate.getMinutes()).padStart(2, '0');
+        return(`${binYear}-${binMonth}-${binDay}T${binHour}:${binMinute}`);
+    }
+    function handleEventModal(data) {
+        const bin = data.event
+        setShowEvent({
+            id:Number(bin.id),
+            title: bin.title,
+            start: formatDateTime(bin.start),
+            end:formatDateTime(bin.end)
+
+    })
+        setShowEventModal(true)
+
+
+
+        //setIdToDelete(Number(data.event.id))
+    }
+
     function handleDelete() {
         setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)))
         setShowDeleteModal(false)
@@ -122,6 +154,13 @@ export default function Calendar() {
         })
         setShowDeleteModal(false)
         setIdToDelete(null)
+        setShowEventModal(false)
+        setShowEvent({
+            title: '',
+            start: '',
+            end: '',
+            id: 0,
+        })
     }
 
     const handleChange = (e) => {
@@ -135,6 +174,19 @@ export default function Calendar() {
         })
     }
 
+    const handleEventChange = (e) => {
+        // populates event object (generically from all inputs)
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        setShowEvent({
+            ...showEvent,
+            [name]: value,
+        })
+
+        console.log(showEvent)
+    }
+
     function handleSubmit(e) {
         e.preventDefault()
         console.log("neues event ",newEvent)
@@ -146,6 +198,21 @@ export default function Calendar() {
             id: 0
         })
     }
+
+    function handleEdit(e) {
+        e.preventDefault()
+        console.log("geupdatedes event ",showEvent)
+        //logic
+        setShowEventModal(false)
+
+        setShowEvent({
+            title: '',
+            start: '',
+            end: '',
+            id: 0,
+        })
+    }
+
 
     return (
         <>
@@ -174,7 +241,7 @@ export default function Calendar() {
                             selectMirror={true}
                             dateClick={handleDateClick}
                             drop={(data) => addEvent(data)}
-                            eventClick={(data) => handleDeleteModal(data)}
+                            eventClick={(data) => handleEventModal(data)}
                         />
                     </div>
                     <div id="draggable-el" className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
@@ -320,6 +387,82 @@ export default function Calendar() {
                                                             disabled={newEvent.title === ''}
                                                         >
                                                             Create
+                                                        </button>
+                                                        <button
+                                                            type="button"
+                                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                                                            onClick={handleCloseModal}
+
+                                                        >
+                                                            Cancel
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </Dialog.Panel>
+                                </Transition.Child>
+                            </div>
+                        </div>
+                    </Dialog>
+                </Transition.Root>
+                <Transition.Root show={showEventModal} as={Fragment}>
+                    <Dialog as="div" className="relative z-10" onClose={setShowEventModal}>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 z-10 overflow-y-auto">
+                            <div
+                                className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                >
+                                    <Dialog.Panel
+                                        className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                        <div>
+                                            <div
+                                                className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                                <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true"/>
+                                            </div>
+                                            <div className="mt-3 text-center sm:mt-5">
+                                                <Dialog.Title as="h3"
+                                                              className="text-base font-semibold leading-6 text-gray-900">
+                                                    {showEvent.title}
+                                                </Dialog.Title>
+                                                <form action="submit" onSubmit={handleEdit}>
+                                                    <div className="mt-2">
+                                                        <input type="datetime-local" name="start"
+                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                                               value={showEvent.start} onChange={(e) => handleEventChange(e)}/>
+                                                    </div>
+                                                    <div className="mt-2">
+                                                        <input type="datetime-local" name="end"
+                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                                               value={showEvent.end} onChange={(e) => handleEventChange(e)}/>
+                                                    </div>
+                                                    <div
+                                                        className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                                        <button
+                                                            type="submit"
+                                                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                                                            disabled={showEvent.title === '' || showEvent.start === '' || showEvent.end === '' }
+                                                        >
+                                                            Edit
                                                         </button>
                                                         <button
                                                             type="button"
