f96b1f01824081ccd6615c767bfec13a82735cca
github4touchdouble <touchedouble@gmx.de>
2023-10-01 20:14:31 +0200

    tidied up

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -5,28 +5,37 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, {Draggable, DropArg} from "@fullcalendar/interaction"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import {Dialog, Transition} from '@headlessui/react'
-import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid'
-import { EventSourceInput } from '@fullcalendar/core/index.js'
-
+import {CheckIcon, ExclamationTriangleIcon} from '@heroicons/react/20/solid'
+import {EventSourceInput} from '@fullcalendar/core/index.js'
 
 
 export default function Calendar() {
     const [events, setEvents] = useState([
-        {title: 'event 1', id: '1'},
-        {title: 'event 2', id: '2'},
-        {title: 'event 3', id: '3'},
-        {title: 'event 4', id: '4'},
-        {title: 'event 5', id: '5'},
+        {title: 'event 1', id: '1', start: '2023-08-29 06:16:28', end: '2023-08-29 16:00:00'},
+        {title: 'event 2', id: '2', start: '2023-08-30 06:16:28', end: '2023-08-30 16:00:00'},
+        {title: 'event 3', id: '3', start: '2023-08-31 06:16:28', end: '2023-08-31 16:00:00'},
+        {title: 'event 4', id: '4', start: '2023-09-01 06:16:28', end: '2023-09-01 16:00:00'},
+        {title: 'event 5', id: '5', start: '2023-09-02 06:16:28', end: '2023-09-02 16:00:00'},
     ])
-    const [allEvents, setAllEvents] = useState([])
+    const [allEvents, setAllEvents] = useState([{
+        title: 'event 1',
+        id: '1',
+        start: '2023-08-29 06:16:28',
+        end: '2023-08-29 16:00:00'
+    },
+        {title: 'event 2', id: '2', start: '2023-08-30 06:16:28', end: '2023-08-30 16:00:00'},
+        {title: 'event 3', id: '3', start: '2023-08-31 06:16:28', end: '2023-08-31 16:00:00'},
+        {title: 'event 4', id: '4', start: '2023-09-01 06:16:28', end: '2023-09-01 16:00:00'},
+        {title: 'event 5', id: '5', start: '2023-09-02 06:16:28', end: '2023-09-02 16:00:00'}])
     const [showModal, setShowModal] = useState(false)
     const [showDeleteModal, setShowDeleteModal] = useState(false)
     const [idToDelete, setIdToDelete] = useState(null)
     const [newEvent, setNewEvent] = useState({
         title: '',
         start: '',
+        end: '',
         allDay: false,
-        id: 0
+        id: 0,
     })
 
     useEffect(() => {
@@ -48,7 +57,7 @@ export default function Calendar() {
     }, [])
 
     function handleDateClick(arg) {
-        setNewEvent({...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime()})
+        setNewEvent({...newEvent, start: arg.start, allDay: arg.allDay, id: new Date().getTime()})
         setShowModal(true)
     }
 
@@ -57,7 +66,7 @@ export default function Calendar() {
             ...newEvent,
             start: data.date.toISOString(),
             title: data.draggedEl.innerText,
-            allDay: data.allDay,
+            allDay: false,
             id: new Date().getTime()
         }
         setAllEvents([...allEvents, event])
@@ -86,17 +95,41 @@ export default function Calendar() {
         setIdToDelete(null)
     }
 
-    const handleChange = (e) => {
+    const handleTitleChange = (e) => {
+        console.log("before: ", newEvent);
         setNewEvent({
             ...newEvent,
-            title: e.target.value
+            title: e.target.value,
+        })
+        console.log("after: ", newEvent);
+        console.log("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|");
+    }
+
+    const handleStartChange = (e) => {
+
+        setNewEvent({
+            ...newEvent,
+            start: e.target.value,
+        })
+
+    }
+
+    const handleChange = (e) => {
+        // populates event object (generically from all inputs)
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        setNewEvent({
+            ...newEvent,
+            [name]: value,
         })
     }
 
     function handleSubmit(e) {
         e.preventDefault()
+        console.log(e)
         setAllEvents([...allEvents, newEvent])
-        setShowModal(false)
+        setShowModal(false) // schließt dialog fenster
         setNewEvent({
             title: '',
             start: '',
@@ -104,6 +137,7 @@ export default function Calendar() {
             id: 0
         })
     }
+
     return (
         <>
             <nav className="flex justify-between mb-12 border-b border-gray-700 p-4">
@@ -121,7 +155,7 @@ export default function Calendar() {
                             headerToolbar={{
                                 left: 'prev,next today',
                                 center: 'title',
-                                right: 'resourceTimelineWook, dayGridMonth,timeGridWeek'
+                                right: 'resourceTimelineWook, dayGridMonth,timeGridWeek,timeGridDay'
                             }}
                             events={allEvents}
                             nowIndicator={true}
@@ -159,11 +193,12 @@ export default function Calendar() {
                             leaveTo="opacity-0"
 
                         >
-                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                         </Transition.Child>
 
                         <div className="fixed inset-0 z-10 overflow-y-auto">
-                            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <div
+                                className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                 <Transition.Child
                                     as={Fragment}
                                     enter="ease-out duration-300"
@@ -180,10 +215,12 @@ export default function Calendar() {
                                             <div className="sm:flex sm:items-start">
                                                 <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center
                       justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600"
+                                                                             aria-hidden="true"/>
                                                 </div>
                                                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                                    <Dialog.Title as="h3"
+                                                                  className="text-base font-semibold leading-6 text-gray-900">
                                                         Delete Event
                                                     </Dialog.Title>
                                                     <div className="mt-2">
@@ -212,6 +249,96 @@ export default function Calendar() {
                         </div>
                     </Dialog>
                 </Transition.Root>
+                <Transition.Root show={showModal} as={Fragment}>
+                    <Dialog as="div" className="relative z-10" onClose={setShowModal}>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 z-10 overflow-y-auto">
+                            <div
+                                className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                >
+                                    <Dialog.Panel
+                                        className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                        <div>
+                                            <div
+                                                className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                                <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true"/>
+                                            </div>
+                                            <div className="mt-3 text-center sm:mt-5">
+                                                <Dialog.Title as="h3"
+                                                              className="text-base font-semibold leading-6 text-gray-900">
+                                                    Add Event
+                                                </Dialog.Title>
+                                                <form action="submit" onSubmit={handleSubmit}>
+                                                    <div className="mt-2">
+                                                        <input type="text" name="title"
+                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                                               value={newEvent.title} onChange={(e) => handleChange(e)} placeholder="Title"/>
+                                                    </div>
+                                                    <div className="mt-2">
+                                                        <input type="datetime-local" name="start"
+                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                                               value={newEvent.start} onChange={(e) => handleChange(e)} placeholder="Start"/>
+                                                    </div>
+                                                    <div
+                                                        className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                                        <button
+                                                            type="submit"
+                                                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                                                            disabled={newEvent.title === ''}
+                                                        >
+                                                            Create
+                                                        </button>
+                                                        <button
+                                                            type="button"
+                                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                                                            onClick={handleCloseModal}
+
+                                                        >
+                                                            Cancel
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </Dialog.Panel>
+                                </Transition.Child>
+                            </div>
+                        </div>
+                    </Dialog>
+                </Transition.Root>
+
+            </main>
+            <div>
+                <p>This is a content to make our page longer</p>
+                <div className="w-full h-screen"></div>
+                <p>
+                    Lorem Ipsum is simply dummy text ...
+                </p>
+            </div>
+        </>
+    );
+}
+
+/*
                 <Transition.Root show={showModal} as={Fragment}>
                     <Dialog as="div" className="relative z-10" onClose={setShowModal}>
                         <Transition.Child
@@ -281,14 +408,5 @@ export default function Calendar() {
                         </div>
                     </Dialog>
                 </Transition.Root>
-            </main >
-            <div>
-                <p>This is a content to make our page longer</p>
-                <div className="w-full h-screen"></div>
-                <p>
-                    Lorem Ipsum is simply dummy text ...
-                </p>
-            </div>
-        </>
-    );
-}
+
+ */
