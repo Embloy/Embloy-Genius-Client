3290d758df1f4a297ec5896e69ef6b4568dfc3f7
github4touchdouble <touchedouble@gmx.de>
2023-12-15 09:28:24 +0100

    added structure for profile dection

--- app/settings/profile.tsx
+++ app/settings/profile.tsx
@@ -8,6 +8,7 @@ import {ClientTokenClaim} from "@/app/settings/ClientTokenClaim";
 import {ChangePassword} from "@/app/settings/ChangePassword";
 import {TwoFactorAuthentication} from "@/app/settings/TwoFactorAuthentication";
 import {UserContext} from "@/app/components/misc/UserContext";
+import Image from "next/image";
 
 
 function TokenClaimScaffold({title, pre_text, link_url, link_text, post_text, button_text, child}) {
@@ -46,7 +47,7 @@ function TokenClaimScaffold({title, pre_text, link_url, link_text, post_text, bu
                 {clicked ? (
                     <button onClick={handleClose}
                             className="px-4 py-1 rounded-full flex items-center justify-center bg-black border-[2px] border-gray-400 hover:border-gray-200 text-gray-400 hover:text-gray-200">
-                        <p className>Close</p>
+                        <p >Close</p>
                     </button>) : (
                     <button onClick={handleRequest}
                             className={cn(disableRequest ? "px-4 py-1 rounded-full flex items-center justify-center border-[2px] border-transparent bg-gray-700 cursor-not-allowed" : "px-4 py-1 rounded-full flex items-center justify center border-[2px] border-transparent bg-embloy-purple-light hover:bg-embloy-purple-lighter")}>
@@ -76,6 +77,34 @@ function TokenClaimScaffold({title, pre_text, link_url, link_text, post_text, bu
     )
 }
 
+function ProfileInfo(){
+    return (
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+            <div className="w-full flex flex-row items-center justify-between">
+                <div>
+
+                </div>
+                <div>
+                    <div className="relative inline-block">
+                        <Image
+                            src="https://about.embloy.com/assets/banner_2-38f470bc.png"
+                            alt="Logo"
+                            height="50"
+                            width="50"
+                            className="rounded-full w-40 h-40"
+                        />
+                        <button
+                            className="absolute bottom-4 left-4 px-4 py-2 bg-black border border-embloy-purple-light text-embloy-purple-light rounded-lg cursor-pointer"
+                        >
+                            Edit
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export function ProfileSettings() {
     let user = useContext(UserContext)
@@ -97,6 +126,7 @@ export function ProfileSettings() {
 
                 <div
                     className="text-sm text-gray-400 w-full flex flex-col items-start justify-start gap-4 border-b border-gray-700 p-4">
+                    <ProfileInfo />
                     <TokenClaimScaffold
                         title="Access Token"
                         pre_text="Access Tokens are used for every interaction with the"
