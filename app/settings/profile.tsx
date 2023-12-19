e81eb6b5cda55ed42648354b6c4c486be6782f90
Winston Smith <touchedouble@gmx.de>
2023-12-19 17:44:13 +0100

    added notification settings

--- app/settings/profile.tsx
+++ app/settings/profile.tsx
@@ -2,9 +2,6 @@
 import './locals.css'
 
 import React, {useContext, useEffect, useRef, useState} from "react";
-import {cn} from "@/lib/utils";
-import {AccessTokenClaim} from "@/app/settings/AccessTokenClaim";
-import {ClientTokenClaim} from "@/app/settings/ClientTokenClaim";
 import {ChangePassword} from "@/app/settings/ChangePassword";
 import {TwoFactorAuthentication} from "@/app/settings/TwoFactorAuthentication";
 import {UserContext} from "@/app/components/misc/UserContext";
@@ -12,71 +9,7 @@ import Image from "next/image";
 import {NotificationSettings} from "@/app/settings/NotificationSettings";
 
 
-function TokenClaimScaffold({title, pre_text, link_url, link_text, post_text, button_text, child}) {
-    const [clicked, setClicked] = useState(false);
-    const [disableRequest, setDisableRequest] = useState(false);
-    const [timeOutID, setTimeOutID] = useState(null);
 
-    const handleRequest = () => {
-        if (!disableRequest) {
-            setClicked(true);
-            setDisableRequest(true);
-            const id = setTimeout(() => {
-                setDisableRequest(false);
-            }, 60000); // in milliseconds => 1 min
-            setTimeOutID(id);
-        }
-    }
-
-    const handleClose = () => {
-        setClicked(false);
-    }
-
-    useEffect(() => {
-        return () => {
-            if (timeOutID) {
-                clearTimeout(Number(timeOutID)); // Clear the timeout on component unmount
-            }
-        };
-    }, [timeOutID]);
-
-
-    return (
-        <div className="w-full flex flex-col items-start justify-start gap-4">
-            <div className="w-full flex flex-row items-center justify-between">
-                <h1 className="text-lg font-medium">{title}</h1>
-                {clicked ? (
-                    <button onClick={handleClose}
-                            className="px-4 py-1 rounded-full flex items-center justify-center bg-black border-[2px] border-gray-400 hover:border-gray-200 text-gray-400 hover:text-gray-200">
-                        <p>Close</p>
-                    </button>) : (
-                    <button onClick={handleRequest}
-                            className={cn(disableRequest ? "px-4 py-1 rounded-full flex items-center justify-center border-[2px] border-transparent bg-gray-700 cursor-not-allowed" : "px-4 py-1 rounded-full flex items-center justify center border-[2px] border-transparent bg-embloy-purple-light hover:bg-embloy-purple-lighter")}>
-                        {disableRequest ? (
-                            <p className="text-gray-400">Disabled</p>
-                        ) : (
-                            <p className="text-white">{button_text}</p>
-                        )}
-
-                    </button>
-                )}
-            </div>
-            <div className="flex flex-row items-center justify-start">
-                <p className="text-gray-400">{pre_text}</p>
-                <div className="w-1"/>
-                <a className="italic text-embloy-purple-lighter hover:underline cursor-pointer"
-                   href={link_url}><p>{link_text}</p></a>
-                <div className="w-1"/>
-                <p>{post_text}</p>
-            </div>
-            {clicked && (
-                <div className="w-full">
-                    {child}
-                </div>
-            )}
-        </div>
-    )
-}
 
 function ProfileInfo() {
     let user = useContext(UserContext)
@@ -163,6 +96,11 @@ function ProfileInfo() {
     return (
 
         <div className="w-full flex flex-col items-start justify-start gap-4">
+            <div className="w-full flex flex-row items-center justify-start gap-3">
+                <div className="border border-gray-700 bg-black px-2 rounded-full">
+                    <p className="text-gray-700 text-xs">Functionality disabled</p>
+                </div>
+            </div>
 
             {user ? (
                 <div className="flex flex-row items-start justify-start border border-gray-700 rounded-lg mb-6 px-4 py-2 gap-10">
@@ -203,7 +141,9 @@ function ProfileInfo() {
                                 <p className="w-[200px] left px-4 text-white">{firstName == '' ? user.first_name : firstName} {lastName == '' ? user.last_name : lastName}</p>
                                 {nameIsHovered && (
                                     <button onClick={nameClick}
-                                            className="text-xs italic text-gray-700 hover:underline cursor-pointer">
+                                            className="text-xs italic text-gray-700 hover:underline cursor-not-allowed"
+                                            disabled={true}
+                                    >
                                         <p>Edit</p>
                                     </button>
                                 )}
@@ -232,7 +172,9 @@ function ProfileInfo() {
                                 <p className="w-[200px] left px-4 text-white">{email == '' ? user.email : email}</p>
                                 {emailIsHovered && (
                                     <button onClick={emailClick}
-                                            className="text-xs italic text-gray-700 hover:underline cursor-pointer">
+                                            className="text-xs italic text-gray-700 hover:underline cursor-not-allowed "
+                                            disabled={true}
+                                    >
                                         <p>Edit</p>
                                     </button>
                                 )}
@@ -264,7 +206,9 @@ function ProfileInfo() {
                             className="rounded-full w-40 h-40 border border-gray-700"
                         />
                         <button
-                            className="absolute bottom-4 left-4 px-4 py-1 bg-black border-[2px] border-embloy-purple-light hover:border-embloy-purple-lighter text-embloy-purple-light hover:text-embloy-purple-lighter rounded-full cursor-pointer"
+                            className="absolute bottom-4 left-4 px-4 py-1 bg-black border-[2px] rounded-full cursor-not-allowed border-gray-700 text-gray-700"
+                            disabled={true}
+                            id="border-embloy-purple-light hover:border-embloy-purple-lighter text-embloy-purple-light hover:text-embloy-purple-lighter"
                         >
                             Edit
                         </button>
