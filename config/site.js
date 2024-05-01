fe6dbbe1674d96316ef38013f6aee75a6798c073
carlobortolan <carlobortolan@gmail.com>
2024-05-02 01:02:40 +0200

    Update dependencies

--- config/site.js
+++ config/site.js
@@ -4,8 +4,6 @@ export const siteConfig = {
     api_url: "https://api2.embloy.com", // <-- Genius Server (remote)
     // core_api_url: "https://api.embloy.com/api/v0", // <-- Core Server (remote)
     core_api_url: "http://localhost:3000/api/v0", // <-- Core Server (local)
-    // core_api_root_url: "https://api.embloy.com", // <-- Core Server (remote)
-    core_api_root_url: "https://localhost:3001", // <-- Core Server (local)
     links: {
         core: "https://embloy.com",
         developer: "https://developers.embloy.com",
