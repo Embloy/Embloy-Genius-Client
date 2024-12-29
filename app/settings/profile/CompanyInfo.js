32e5b84527b75b82d545803a6f168a0bb6a86cf7
jh <jan.hummel@de.embloy.com>
2024-12-29 22:22:23 +0100

    working commit

--- app/settings/profile/CompanyInfo.js
+++ app/settings/profile/CompanyInfo.js
@@ -9,8 +9,45 @@ import { EmbloyInput, EmbloyInputbox, EmbloyInputboxElement, EmbloyRadioOption, 
 import { EmbloyP, EmbloyH1} from "@/app/components/ui/misc/text";
 import { XIcon } from "lucide-react";
 import { FaLink } from "react-icons/fa";
-import { CompanyDescription } from "./CompanyDescription";
-
+import Link from "next/link";
+import { siteConfig } from "@/config/site";
+import {marked} from "marked";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import TurndownService from 'turndown';
+import { html_to_markdown, checkBody } from "@/app/recruitment/post_details";
+import parse, { domToReact } from 'html-react-parser';
+const options = {
+    replace: (domNode) => {
+        if (domNode.name === "h1") {
+            return <EmbloyH1 className="text-base mb-4">{domToReact(domNode.children, options)}</EmbloyH1>;
+        }
+        if (domNode.name === "p") {
+            return <EmbloyP className="text-sm mb-4">{domToReact(domNode.children, options)}</EmbloyP>;
+        }
+        if (domNode.name === "ul") {
+            return <ul className="mb-4">{domToReact(domNode.children, options)}</ul>;
+        }
+        if (domNode.name === "li") {
+            return <li className="text-sm">{domToReact(domNode.children, options)}</li>;
+        }
+        if (domNode.name === "strong") {
+            return <strong>{domToReact(domNode.children, options)}</strong>;
+        }
+        if (domNode.name === "a") {
+            return (
+                <a
+                    href={domNode.attribs.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 underline"
+                >
+                    {domToReact(domNode.children)}
+                </a>
+            );
+        }
+    },
+};
 const industries = [
     {value: "Technology", label: "Technology"},
     {value: "Healthcare", label: "Healthcare"},
@@ -54,6 +91,7 @@ const industries = [
 
 export function CompanyInfo(reload) {
     let {user, company, subscription} = useContext(UserContext)
+    const [body, setBody] = useState(company.company_description.body);
 
     const [newImageUrl, setNewImageUrl] = useState(null);
     const [avatar, set_avatar] = useState(false);
@@ -247,6 +285,77 @@ export function CompanyInfo(reload) {
             set_avatar(true);
         }
     }, [company]);
+
+    const [showDesc, setShowDesc] = useState(false);
+    let bin = html_to_markdown(company?.company_description?.body);
+    const [markdown, setMarkdown] = useState({ md: bin || "", cs: { line: 0, ch: 0 } });
+    const [previewStatus, setPreviewStatus] = useState(false);
+
+    const handlePreview = () => {
+        setPreviewStatus(!previewStatus);
+    };
+
+    const inactivityTimer = useRef(null);
+    const [timeLeft, setTimeLeft] = useState(null);
+
+    const resetInactivityTimer = () => {
+        /*
+        if (inactivityTimer.current) {
+            clearInterval(inactivityTimer.current);
+        }
+
+        let timeRemaining = 1; // Start with 2 seconds
+        setTimeLeft(timeRemaining);
+
+        inactivityTimer.current = setInterval(() => {
+            if (timeRemaining > 0) {
+                timeRemaining -= 0.5; // Decrement by 0.5 seconds
+                setTimeLeft(timeRemaining);
+            } else {
+                clearInterval(inactivityTimer.current);
+                handleDescriptionSave();
+            }
+        }, 500);
+        */
+    };
+
+    useEffect(() => {
+        /*
+        return () => {
+            if (inactivityTimer.current) {
+                clearInterval(inactivityTimer.current);
+            }
+        };
+        */
+    }, []);
+
+    const handleDescriptionSave = async () => {
+        if (checkBody(body, marked(markdown.md))) {
+            console.log("DESCRIPTION SAVE", body, marked(markdown.md))
+            let desc = { ...company.company_description };
+            desc.body = marked(markdown.md);
+            if (desc.body === "") {
+                desc.body = null;
+            }
+            const req_body = {
+                "company_description": desc.body
+            }
+            const cs = markdown.cs;
+            setPreviewStatus(false);
+            try {
+                const res = await not_core_get("PATCH", `/company/${company.id}`, req_body);
+                console.log("BEFORE",checkBody(body, marked(markdown.md)))
+                setBody(marked(markdown.md))
+                console.log("AFTER",checkBody(body, marked(markdown.md)))
+                setMarkdown({ md: markdown.md, cs: cs});
+                console.log("DESCRIPTION SAVE", res)
+
+            } catch (error) {
+                setMarkdown(body, cs); // Roll back on error
+                console.log("DESCRIPTION SAVE ERROR", error)    
+            }
+        } 
+    };
     
 
 
@@ -476,6 +585,101 @@ export function CompanyInfo(reload) {
                                 </button>
                                 
                             </EmbloyH>
+                        </EmbloyInputboxElement>
+                    </EmbloyInputbox>
+                    <EmbloyInputbox>
+                        <EmbloyInputboxElement head="Company Description" description="Customize the Company Board">
+           
+                            <EmbloyH className="gap-2" >
+                                <Link href={`${siteConfig.core_url}/board/${company.company_slug}`} target="_blank">
+                                    <EmbloyButton name="To Company Board" onClick={() => {}} variant="bold" />
+                                </Link>
+                                <EmbloyButton name="Edit Company Board" onClick={() => setShowDesc(true)} variant="bold" />
+                                {
+                                    showDesc && (
+
+                                        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800/75">
+                                            <div className="w-11/12 rounded-md bg-white px-6 py-3 shadow-md dark:bg-gray-900 md:w-3/4">
+                                                <div className="flex flex-row items-start justify-end">
+                                                    <button
+                                                        onClick={() => {
+                                                            setShowDesc(false)
+                                                            handleDescriptionSave()
+                                                        }}
+                                                        className="font-heading text-xl text-gray-800 dark:text-gray-200"
+                                                    >
+                                                        &times;
+                                                    </button>
+                                                </div>
+                                                <EmbloyH className="justify-between items-end">
+                                                    <EmbloyH1 className="font-heading text-base">Company Description:</EmbloyH1>
+                                                </EmbloyH>
+                                                <div className="mt-0.5 mb-2 h-p" />
+                                                { !previewStatus ?
+                                                    <SimpleMDE
+                                                        value={markdown.md}
+                                                        getCodemirrorInstance={(instance) => { 
+                                                            instance.on('change', (editor) => {
+                                                                const cursorPosition = editor.getCursor(); 
+                                                                const value = editor.getValue(); 
+                                                                setMarkdown({ md: value, cs: cursorPosition }); 
+                                                                resetInactivityTimer(); // Reset the inactivity timer on typing
+                                                            });
+                                                            if (markdown.cs) {
+                                                                instance.setCursor(markdown.cs);
+                                                            }
+                                                        }}
+                                                        className="w-full text-black dark:text-white"
+                                                        options={{
+                                                            autofocus: true,
+                                                            hideIcons: ["guide", "side-by-side", "fullscreen", "quote", "preview"],
+                                                            toolbar: [
+                                                                {
+                                                                    name: "custom",
+                                                                    action: function customFunction(editor) {
+                                                                        handlePreview();
+                                                                    },
+                                                                    className: "fa fa-eye",
+                                                                    title: "Custom Button",
+                                                                },
+                                                                "|",
+                                                                "bold",
+                                                                "italic",
+                                                                "heading",
+                                                                "unordered-list",
+                                                                "ordered-list",
+                                                                "link",
+                                                                "image",
+                                                                "|",
+                                                                {
+                                                                    name: "custom_save",
+                                                                    action: function customFunction(editor) {
+                                                                        handleDescriptionSave();
+                                                                    },
+                                                                    className: "fa fa-save",
+                                                                    title: "Save Button",
+                                                                },
+                                                            ],
+                                                        }}
+                                                    />
+                                                : 
+                                                <EmbloyV className="border border-etna dark:border-nebbiolo rounded-md ">
+                                                    <EmbloyH className={"justify-start gap-2 px-4 py-2"}>
+                                                        <button onClick={handlePreview}>
+                                                            <EmbloyP className="text-xs text-capri dark:text-capri underline">{"Return to Editor"}</EmbloyP>
+                                                        </button>
+                                                    </EmbloyH>
+                                                    <EmbloySeperator className="h-px w-full bg-etna dark:bg-nebbiolo" />
+                                                    <EmbloyV className="px-4 py-2">
+                                                        {markdown && parse(body || '', options)}
+                                                    </EmbloyV>
+                                                </EmbloyV>
+                                                }
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </ EmbloyH>
                         </EmbloyInputboxElement>
                     </EmbloyInputbox>
                 </EmbloyV>
